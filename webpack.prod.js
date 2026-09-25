dae1717ab1a866ae91d6f1a2b75bcecb11afe2f7
sohaib <rajashahzaibabdullah8467@gmail.com>
2026-09-25 06:07:58 +0500

    Install css loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,22 @@
 import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // Overrides the common style-loader with the extraction loader
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
 });
